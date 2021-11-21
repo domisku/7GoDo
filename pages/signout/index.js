c5d8c5c97e561c62ab8e831ccbcc012a2a127f20
Dom Sku <dskuodziunas@gmail.com>
2021-11-21 13:45:56 +0200

    added loading screen to sign out page

--- pages/signout/index.js
+++ pages/signout/index.js
@@ -3,39 +3,49 @@ import { faForward } from "@fortawesome/free-solid-svg-icons";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import Head from "next/dist/shared/lib/head";
+import { useState } from "react";
+import Loading from "../../components/UI/Loading";
 
 function SignOut() {
-  return (
-    <>
-      <Head>
-        <title>Sign Out</title>
-        <meta name="description" content="Sign out of your account" />
-      </Head>
-      <div className="flex justify-center items-center h-screen bg-gray-50">
-        <div className="relative xl:-top-16 w-full sm:w-9/12 md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 bg-white p-8 sm:p-14 border rounded-lg shadow-lg">
-          <Link href="/">
-            <h1 className="text-2xl text-red-500 cursor-pointer mb-4 font-medium">
-              7GoDo
-              <Icon className="ml-1" icon={faForward} />
-            </h1>
-          </Link>
-          <h2 className="text-3xl font-bold mb-2">Sign Out</h2>
-          <p className="text-xl mb-8">Are you sure you want to sign out?</p>
-          <button
-            onClick={() => signOut({ callbackUrl: process.env.NEXTAUTH_URL })}
-            className="flex items-center justify-center w-full mb-6 px-4 py-2 bg-red-500 font-medium text-lg rounded-lg text-white hover:opacity-90"
-          >
-            Sign Out
-          </button>
-          <Link href="/">
-            <p className="text-red-500 cursor-pointer hover:underline">
-              Go Home
-            </p>
-          </Link>
+  const [loading, setLoading] = useState(false);
+
+  if (loading) return <Loading />;
+
+  if (!loading)
+    return (
+      <>
+        <Head>
+          <title>Sign Out</title>
+          <meta name="description" content="Sign out of your account" />
+        </Head>
+        <div className="flex justify-center items-center h-screen bg-gray-50">
+          <div className="relative xl:-top-16 w-full sm:w-9/12 md:w-8/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 bg-white p-8 sm:p-14 border rounded-lg shadow-lg">
+            <Link href="/">
+              <h1 className="text-2xl text-red-500 cursor-pointer mb-4 font-medium">
+                7GoDo
+                <Icon className="ml-1" icon={faForward} />
+              </h1>
+            </Link>
+            <h2 className="text-3xl font-bold mb-2">Sign Out</h2>
+            <p className="text-xl mb-8">Are you sure you want to sign out?</p>
+            <button
+              onClick={() => {
+                signOut({ callbackUrl: process.env.NEXTAUTH_URL });
+                setLoading(true);
+              }}
+              className="flex items-center justify-center w-full mb-6 px-4 py-2 bg-red-500 font-medium text-lg rounded-lg text-white hover:opacity-90"
+            >
+              Sign Out
+            </button>
+            <Link href="/">
+              <p className="text-red-500 cursor-pointer hover:underline">
+                Go Home
+              </p>
+            </Link>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
 
 export default SignOut;
