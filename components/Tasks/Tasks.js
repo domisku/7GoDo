9b3795e6c863ee90d5162062a0b553cc29a22745
Dom Sku <dskuodziunas@gmail.com>
2021-11-22 13:33:11 +0200

    added loading screen until CRUD operations are completed in the database

--- components/Tasks/Tasks.js
+++ components/Tasks/Tasks.js
@@ -14,6 +14,7 @@ import deleteTask from "../../utils/deleteTask";
 import useSWR, { useSWRConfig } from "swr";
 import router from "next/router";
 import Spinner from "../UI/Spinner";
+import Loading from "../UI/Loading";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -27,6 +28,7 @@ function Tasks(props) {
 
   const [input, setInput] = useState("");
   const [taskData, setTaskData] = useState("");
+  const [loading, setLoading] = useState(false);
 
   let audio = new Audio("/ping.mp3");
 
@@ -87,6 +89,8 @@ function Tasks(props) {
         audio.play();
       }
 
+      setLoading(true);
+
       mutate(
         `/api/${session.user.name || session.user.email}`,
         [...updatedData, {}],
@@ -105,6 +109,8 @@ function Tasks(props) {
       if (response.status !== 200) router.push("/error");
 
       mutate(`/api/${session.user.name || session.user.email}`);
+
+      setLoading(false);
     }
   }
 
@@ -118,6 +124,8 @@ function Tasks(props) {
   async function taskDeletedHandler(id) {
     const filteredData = data.filter((task) => task._id !== id);
 
+    setLoading(true);
+
     mutate(
       `/api/${session.user.name || session.user.email}`,
       [...filteredData],
@@ -130,6 +138,8 @@ function Tasks(props) {
     if (status !== 200) router.push("/error");
 
     mutate(`/api/${session.user.name || session.user.email}`);
+
+    setLoading(false);
   }
 
   async function markAsImportantHandler(event) {
@@ -147,6 +157,8 @@ function Tasks(props) {
         return task;
       });
 
+      setLoading(true);
+
       mutate(
         `/api/${session.user.name || session.user.email}`,
         [...updatedData, {}],
@@ -161,6 +173,8 @@ function Tasks(props) {
       if (response.status !== 200) router.push("/error");
 
       mutate(`/api/${session.user.name || session.user.email}`);
+
+      setLoading(false);
     }
   }
 
@@ -172,6 +186,8 @@ function Tasks(props) {
         }
         return task;
       });
+
+      setLoading(true);
 
       mutate(
         `/api/${session.user.name || session.user.email}`,
@@ -191,6 +207,8 @@ function Tasks(props) {
       if (response.status !== 200) router.push("/error");
 
       mutate(`/api/${session.user.name || session.user.email}`);
+
+      setLoading(false);
     }
   }
 
@@ -202,6 +220,8 @@ function Tasks(props) {
         }
         return task;
       });
+
+      setLoading(true);
 
       mutate(
         `/api/${session.user.name || session.user.email}`,
@@ -221,6 +241,8 @@ function Tasks(props) {
       if (response.status !== 200) router.push("/error");
 
       mutate(`/api/${session.user.name || session.user.email}`);
+
+      setLoading(false);
     }
   }
 
@@ -247,95 +269,158 @@ function Tasks(props) {
       )[0]
     : null;
 
-  return (
-    <Main
-      dataDidLoad={data}
-      taskData={taskData}
-      clearTaskData={clearTaskData}
-      taskCompletedHandler={taskCompletedHandler}
-      taskDeletedHandler={taskDeletedHandler}
-      markAsImportantHandler={markAsImportantHandler}
-      changeTaskNameHandler={changeTaskNameHandler}
-      changeNotesHandler={changeNotesHandler}
-    >
-      <h2 className="w-11/12 break-words text-xl font-medium mb-4">
-        {props.filter === "today"
-          ? "Today"
-          : props.filter === "important"
-          ? "Important"
-          : props.filter === "all"
-          ? "All tasks"
-          : props.listTitle
-          ? props.listTitle
-          : props.date
-          ? [props.date, props.weekday].join(", ")
-          : props.query
-          ? 'Search results for: "' + props.query + '"'
-          : "Tasks"}
-      </h2>
-      {!props.query && (
-        <form
-          className="relative right-2 h-14 flex items-center mr-10 pr-5 pl-4 py-2.5 text-lg bg-white shadow-lg text-red-500 mb-4 rounded-lg"
-          onSubmit={formSubmitHandler}
-        >
-          <button type="submit" disabled={!input}>
-            <Icon className="mr-3" icon={faPlus} fixedWidth></Icon>
-          </button>
-          <input
-            className="w-9/12 outline-none bg-transparent placeholder-red-500"
-            placeholder="New task"
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          ></input>
-        </form>
-      )}
-      <div className={`overflow-y-scroll h-custom pr-6`}>
-        <div>
-          {data &&
-            data
-              .filter(
-                (task) =>
-                  task.status === "ongoing" &&
-                  (props.filter === "important"
-                    ? task.important === "true"
-                    : props.filter === "today"
-                    ? task.date === formatTodaysDate()
-                    : true) &&
-                  (props.listId
-                    ? task.listId === props.listId
-                    : task.listId === null) &&
-                  (props.date ? props.date === task.date : true) &&
-                  (props.query
-                    ? task.task
-                        .toLowerCase()
-                        .includes(props.query.toLowerCase())
-                    : true)
-              )
-              .map((task, index) => {
-                return (
-                  <>
+  if (loading) return <Loading />;
+
+  if (!loading)
+    return (
+      <Main
+        dataDidLoad={data}
+        taskData={taskData}
+        clearTaskData={clearTaskData}
+        taskCompletedHandler={taskCompletedHandler}
+        taskDeletedHandler={taskDeletedHandler}
+        markAsImportantHandler={markAsImportantHandler}
+        changeTaskNameHandler={changeTaskNameHandler}
+        changeNotesHandler={changeNotesHandler}
+      >
+        <h2 className="w-11/12 break-words text-xl font-medium mb-4">
+          {props.filter === "today"
+            ? "Today"
+            : props.filter === "important"
+            ? "Important"
+            : props.filter === "all"
+            ? "All tasks"
+            : props.listTitle
+            ? props.listTitle
+            : props.date
+            ? [props.date, props.weekday].join(", ")
+            : props.query
+            ? 'Search results for: "' + props.query + '"'
+            : "Tasks"}
+        </h2>
+        {!props.query && (
+          <form
+            className="relative right-2 h-14 flex items-center mr-10 pr-5 pl-4 py-2.5 text-lg bg-white shadow-lg text-red-500 mb-4 rounded-lg"
+            onSubmit={formSubmitHandler}
+          >
+            <button type="submit" disabled={!input}>
+              <Icon className="mr-3" icon={faPlus} fixedWidth></Icon>
+            </button>
+            <input
+              className="w-9/12 outline-none bg-transparent placeholder-red-500"
+              placeholder="New task"
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+            ></input>
+          </form>
+        )}
+        <div className={`overflow-y-scroll h-custom pr-6`}>
+          <div>
+            {data &&
+              data
+                .filter(
+                  (task) =>
+                    task.status === "ongoing" &&
+                    (props.filter === "important"
+                      ? task.important === "true"
+                      : props.filter === "today"
+                      ? task.date === formatTodaysDate()
+                      : true) &&
+                    (props.listId
+                      ? task.listId === props.listId
+                      : task.listId === null) &&
+                    (props.date ? props.date === task.date : true) &&
+                    (props.query
+                      ? task.task
+                          .toLowerCase()
+                          .includes(props.query.toLowerCase())
+                      : true)
+                )
+                .map((task, index) => {
+                  return (
+                    <>
+                      <div
+                        key={task._id}
+                        id={task._id}
+                        onClick={taskClickHandler}
+                        className={`w-full ${
+                          taskData._id === task._id ? "h-full" : "h-12"
+                        } pl-4 py-2.5 text-lg cursor-pointer flex items-center rounded-lg ${
+                          index % 2 === 0 ? "bg-gray-100" : "bg-gray-50"
+                        } ${
+                          taskData._id === task._id && task._id
+                            ? "hover:bg-gray-400 bg-gray-400 text-white"
+                            : "hover:bg-gray-200"
+                        }`}
+                      >
+                        <Icon
+                          id={task._id}
+                          onClick={taskCompletedHandler}
+                          className={`mr-3 ${!task._id ? "invisible" : ""}`}
+                          icon={faCircle}
+                          fixedWidth
+                        />
+                        {!task._id && <Spinner />}
+                        <span
+                          className={`${
+                            taskData._id === task._id ? "" : "truncate"
+                          } w-11/12 break-words pointer-events-none`}
+                        >
+                          {task.task}
+                        </span>
+                        {task.important === "true" ? (
+                          <Icon
+                            className="ml-auto mr-4"
+                            icon={faExclamationCircle}
+                            fixedWidth
+                          />
+                        ) : null}
+                      </div>
+                    </>
+                  );
+                })}
+          </div>
+          <h2 className="text-xl font-medium mb-4 mt-4">
+            {filteredDataNotEmpty && "Completed"}
+          </h2>
+          <div className="mb-6">
+            {data &&
+              data
+                .filter(
+                  (task) =>
+                    task.status === "completed" &&
+                    (props.filter === "important"
+                      ? task.important === "true"
+                      : props.filter === "today"
+                      ? task.date === formatTodaysDate()
+                      : true) &&
+                    (props.listId
+                      ? task.listId === props.listId
+                      : task.listId === null) &&
+                    (props.date ? props.date === task.date : true) &&
+                    (props.query
+                      ? task.task
+                          .toLowerCase()
+                          .includes(props.query.toLowerCase())
+                      : true)
+                )
+                .map((task, index) => {
+                  return (
                     <div
                       key={task._id}
                       id={task._id}
                       onClick={taskClickHandler}
-                      className={`w-full ${
+                      className={`line-through w-full ${
                         taskData._id === task._id ? "h-full" : "h-12"
                       } pl-4 py-2.5 text-lg cursor-pointer flex items-center rounded-lg ${
                         index % 2 === 0 ? "bg-gray-100" : "bg-gray-50"
                       } ${
-                        taskData._id === task._id && task._id
+                        taskData._id === task._id
                           ? "hover:bg-gray-400 bg-gray-400 text-white"
                           : "hover:bg-gray-200"
                       }`}
                     >
-                      <Icon
-                        id={task._id}
-                        onClick={taskCompletedHandler}
-                        className={`mr-3 ${!task._id ? "invisible" : ""}`}
-                        icon={faCircle}
-                        fixedWidth
-                      />
-                      {!task._id && <Spinner />}
+                      <Icon className="mr-3" icon={faCheckCircle} fixedWidth />
                       <span
                         className={`${
                           taskData._id === task._id ? "" : "truncate"
@@ -351,72 +436,12 @@ function Tasks(props) {
                         />
                       ) : null}
                     </div>
-                  </>
-                );
-              })}
+                  );
+                })}
+          </div>
         </div>
-        <h2 className="text-xl font-medium mb-4 mt-4">
-          {filteredDataNotEmpty && "Completed"}
-        </h2>
-        <div className="mb-6">
-          {data &&
-            data
-              .filter(
-                (task) =>
-                  task.status === "completed" &&
-                  (props.filter === "important"
-                    ? task.important === "true"
-                    : props.filter === "today"
-                    ? task.date === formatTodaysDate()
-                    : true) &&
-                  (props.listId
-                    ? task.listId === props.listId
-                    : task.listId === null) &&
-                  (props.date ? props.date === task.date : true) &&
-                  (props.query
-                    ? task.task
-                        .toLowerCase()
-                        .includes(props.query.toLowerCase())
-                    : true)
-              )
-              .map((task, index) => {
-                return (
-                  <div
-                    key={task._id}
-                    id={task._id}
-                    onClick={taskClickHandler}
-                    className={`line-through w-full ${
-                      taskData._id === task._id ? "h-full" : "h-12"
-                    } pl-4 py-2.5 text-lg cursor-pointer flex items-center rounded-lg ${
-                      index % 2 === 0 ? "bg-gray-100" : "bg-gray-50"
-                    } ${
-                      taskData._id === task._id
-                        ? "hover:bg-gray-400 bg-gray-400 text-white"
-                        : "hover:bg-gray-200"
-                    }`}
-                  >
-                    <Icon className="mr-3" icon={faCheckCircle} fixedWidth />
-                    <span
-                      className={`${
-                        taskData._id === task._id ? "" : "truncate"
-                      } w-11/12 break-words pointer-events-none`}
-                    >
-                      {task.task}
-                    </span>
-                    {task.important === "true" ? (
-                      <Icon
-                        className="ml-auto mr-4"
-                        icon={faExclamationCircle}
-                        fixedWidth
-                      />
-                    ) : null}
-                  </div>
-                );
-              })}
-        </div>
-      </div>
-    </Main>
-  );
+      </Main>
+    );
 }
 
 export default Tasks;
